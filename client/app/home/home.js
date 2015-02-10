9c5371f77de9f9516c3a1de56c0cf5da187031e4
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-10 14:48:24 -0600

    change landing page

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -4,7 +4,7 @@ angular.module('bigmooncreativeApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('home', {
-        url: '/home',
+        url: '/',
         templateUrl: 'app/home/home.html',
         controller: 'HomeCtrl'
       });
