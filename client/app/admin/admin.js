db91d5f0c2e7cc028ca23c7b8ad8ce973ff7d7cf
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-10 11:13:19 -0600

    first full commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bigmooncreativeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
